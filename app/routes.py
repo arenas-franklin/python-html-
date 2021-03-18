d3468a0b101282a88b204c35096b6c40c335ff42
Franklin Angelo Arenas <franklin.silva8@fatec.sp.gov.br>
2021-03-18 16:39:37 -0300

    adicionando conceito de tamplates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'Franklin'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Suan'},
+            'body': "The avengers movie was so coll!"
+        }
+    ]
+    return render_template("index.html", title='Home', user=user, posts=posts) 
